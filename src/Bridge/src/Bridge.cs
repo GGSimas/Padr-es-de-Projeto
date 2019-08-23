d3b3bd2da0d84329d08401ab6b49b728b3842c69
GGSimas <simoes.gabrielm@gmail.com>
2019-08-22 23:28:44 -0300

    adicionando Adapter/Bridge

---
+++ src/Bridge/src/Bridge.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace src
+{
+    interface WebSite {
+        void acessarNavegador(string nav);
+        void acessarSite(string url);
+
+    }
+
+    abstract class Navegador {
+        protected WebSite website;
+
+        public Navegador(WebSite website){
+            this.website = website;
+        }
+
+        public void acessarNavegador(string nav) {
+            website.acessarNavegador(nav);
+        }
+        public void acessarSite(string url) {
+            website.acessarSite(url);
+        }
+
+        public abstract void acessar();
+    }
+
+    class Chrome: Navegador {
+
+        public Chrome(WebSite webSite): base(webSite) {
+        
+        }
+
+        public override void acessar() {
+            acessarNavegador("Chrome");
+            acessarSite(website.GetType().Name);
+        }
+    }
+
+     class Firefox: Navegador {
+
+        public Firefox(WebSite webSite): base(webSite) {
+        
+        }
+
+        public override void acessar() {
+            acessarNavegador("Firefox");
+            acessarSite(website.GetType().Name);
+        }
+    }
+
+     class Edge: Navegador {
+
+        public Edge(WebSite webSite): base(webSite) {
+        
+        }
+
+        public override void acessar() {
+            acessarNavegador("Edge");
+            acessarSite(website.GetType().Name);
+        }
+    }
+
+    class Facebook : WebSite {
+        public void acessarNavegador(string navegador) {
+            Console.WriteLine(string.Format("Acessando pelo: {0}", navegador));
+        }
+
+        public void acessarSite(string url) {
+            Console.WriteLine(string.Format("Url: {0}", url));
+        }
+    }
+
+    class Youtube : WebSite {
+        public void acessarNavegador(string navegador) {
+            Console.WriteLine(string.Format("Acessando pelo: {0}", navegador));
+        }
+
+        public void acessarSite(string url) {
+            Console.WriteLine(string.Format("Url: {0}", url));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Navegador navegador = new Chrome(new Facebook());
+            navegador.acessar();
+
+            navegador = new Firefox(new Youtube());
+            navegador.acessar();
+        }
+    }
+}
